85bfa6597ad822fdcce9f51c55c6a4524c8148f8
Timothée Hornek <timothee.hornek@web.de>
2021-11-18 22:58:13 +0000

    exercise done

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from setuptools import setup
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="hornektediffusion2D",
-    version="0.0.1",
+    version="0.0.4",
     author="Timothée Hornek",
-    #description="Amazing exercise package",
+    description="simple package for diffusion computation",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/Simulation-Software-Engineering/Diffusion2D-Python-Package",
-    #package_dir={"": "<directory-name>"},
-    #packages=setuptools.find_packages(where="<directory-name>"),
+    packages=setuptools.find_packages(where='src'),
+    package_dir={"": "src"},
+    #packages=setuptools.find_packages(where="./src/hornektediffusion2D/"),
+    #packages=setuptools.find_packages(include=['src/hornektediffusion2D']),
     python_requires=">=3.6",
-    #install_requires=["<installation dependencies>"]
+    install_requires=["numpy", "matplotlib"],
     classifiers=[
         "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: CC BY 4.0"
+        "Operating System :: OS Independent"
+        #"License :: OSI Approved :: CC"
     ]
 )
