106b0292c4dd29beceeab5b443fb844e6e756ac2
Max Hausch <cheriimoya@googlemail.com>
2021-11-24 21:08:41 +0000

    Add readme as long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 import setuptools
 
+with open('README.md') as readme:
+    long_description = readme.read()
+
 setup(
     name="diffusion2d",
     version="0.0.1",
     author="Max Hausch",
     description="Diffusion simulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="package-website-url",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
