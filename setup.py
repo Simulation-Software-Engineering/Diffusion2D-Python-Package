02f876ea5071bcddbd1663e33670dc8470f20b6d
Max Hausch <cheriimoya@googlemail.com>
2021-11-24 21:23:01 +0000

    Expand setup

--- setup.py
+++ setup.py
@@ -5,15 +5,20 @@ with open('README.md') as readme:
     long_description = readme.read()
 
 setup(
-    name="diffusion2d",
+    name="hauschmxdiffusion2D",
     version="0.0.1",
     author="Max Hausch",
     description="Diffusion simulation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="package-website-url",
+    url="https://github.com/Simulation-Software-Engineering",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.8",
-    install_requires=["matplotlib", "numpy"]
+    install_requires=["matplotlib", "numpy"],
+    classifiers=[
+        "Programming Language :: Python :: 3.8.11",
+        "License :: OSI Approved :: CC BY 4.0",
+        "Operating System :: Linux",
+    ],
 )
