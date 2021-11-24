9d465b9aeafbfd97738251af326c71c82d45f876
RPAlessio <st174271@stud.uni-stuttgart.de>
2021-11-25 00:42:34 +0100

    commiting changes after building the package

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name = 'pereirrndiffusion2d',
     version = '0.0.1',
     py_modules = ["diffusion2d"],
-    package_dir = {'src': 'diffusion2d_FTCS'},
+    package_dir = {'': 'src'},
 )
