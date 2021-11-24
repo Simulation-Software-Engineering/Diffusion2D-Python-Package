a0d143f41e2afda90f5c58a5565165df2296d438
Max Hausch <cheriimoya@googlemail.com>
2021-11-24 21:00:58 +0000

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,32 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  diffusion-python = (pkgs.python3.withPackages (ps: with ps; [
+    pip
+    build
+    twine
+    numpy
+    matplotlib
+  ]));
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    diffusion-python
+    autoPatchelfHook
+    zlib
+    gcc-unwrapped.lib
+  ];
+
+  shellHook = ''
+    # export PYTHON_PATH="$(pwd)/.python"
+    # export PIP_PREFIX="$PYTHON_PATH/pip_packages"
+    # mkdir -pv "$PIP_PREFIX"
+    #
+    # export PYTHONPATH="$PYTHON_PATH/pip_packages/lib/python3.8/site-packages:$PYTHONPATH"
+    # export PATH="$PATH:$PIP_PREFIX/bin"
+    #
+    # pip install numpy
+    # pip install matplotlib
+    # autoPatchelf $PYTHON_PATH
+
+    exec zsh
+  '';
+}
